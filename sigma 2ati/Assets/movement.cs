65d66e130842506ee9bb192fc74c0c5e60ee88c9
keelox <kubajanda69@gmail.com>
2025-01-08 09:09:05 +0100

    nikker

---
+++ sigma 2ati/Assets/movement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    [SerializeField] float VectorX;
+    [SerializeField] float VectorY = 0;
+    [SerializeField] float VectorZ;
+    [SerializeField] float Basespeed = 3;
+    [SerializeField] float Currentspeed;
+    [SerializeField] float Sprint = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        VectorX = Input.GetAxis("Horizontal");
+        VectorZ = Input.GetAxis("Vertical");
+        transform.Translate(VectorX * Time.deltaTime*Currentspeed, VectorY * Time.deltaTime*Currentspeed, VectorZ * Time.deltaTime*Currentspeed);
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            Currentspeed = Basespeed + Sprint;
+        }
+        else
+        {
+            Currentspeed = Basespeed;
+        }
+    }
+}
